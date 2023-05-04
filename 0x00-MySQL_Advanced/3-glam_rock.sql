9ebaa5283146aee121de311042b526bc1bff440a
Gideon Obiasor <gideonoiasor@gmail.com>
2023-05-04 22:30:31 +0100

    Task 3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,13 +1,7 @@
 -- Lists all bands with `Glam rock` as their main style,
 -- Ranked by their longevity
 
--- Create a view that changes NULL to 2020 in column split
-CREATE OR REPLACE VIEW old_school_band AS
-SELECT band_name, formed, COALESCE(split, 2023) AS split, style
-FROM metal_bands;
-
--- Use the created view to filter required data
-SELECT band_name, (split - formed) AS lifespan
-FROM old_school_band
+SELECT band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
+FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
