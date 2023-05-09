31f9fcd377636ce80ace81bbfef65f1303770bc3
Gideon Obiasor <gideonoiasor@gmail.com>
2023-05-09 10:14:19 +0100

    Task 12

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" Log stats """
+from pymongo import MongoClient
+
+
+client = MongoClient('mongodb://localhost:27017')
+db = client.logs
+
+log_counts = db.nginx.count_documents({})
+get = db.nginx.count_documents({"method": "GET"})
+post = db.nginx.count_documents({"method": "POST"})
+put = db.nginx.count_documents({"method": "PUT"})
+patch = db.nginx.count_documents({"method": "PATCH"})
+delete = db.nginx.count_documents({"method": "DELETE"})
+status = db.nginx.count_documents({"path": "/status"})
+
+print(f"{log_counts} logs")
+print("Methods:")
+print(f"\tmethod GET: {get}\n\tmethod POST: {post}\n\tmethod PUT: {put}")
+print(f"\tmethod PATCH: {patch}\n\tmethod DELETE: {delete}")
+print(f"{status} status check")
